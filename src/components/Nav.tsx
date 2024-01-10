102ca3f82670d22ac73ec69c502bf65bcff92200
pkstoy13 <pkstoyanov1@gmail.com>
2024-01-10 14:54:37 -0500

    added tailwind and cleaned up file

---
+++ src/components/Nav.tsx
@@ -0,0 +1,12 @@
+export function Nav() {
+  return (
+    <div className="flex justify-end">
+      <button className="mr-4">Overview</button>
+      <button className="mr-4">Education</button>
+      <button className="mr-4">Projects</button>
+      <button className="mr-4">Skills</button>
+      <button className="mr-4">Interests</button>
+      <button className="mr-4">Contact Me</button>
+    </div>
+  );
+}

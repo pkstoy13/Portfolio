0ef0ebb9a153df8cb7ce25b613c4b63a1b1431b3
pkstoy13 <pkstoyanov1@gmail.com>
2024-01-13 00:14:06 -0500

    issues with img imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Nav } from "./components/Nav";
 import { Overview } from "./components/Overview";
-//import pic from "./assets/placeholder.png";
 
 export function App() {
   return (
--- src/components/Overview.tsx
+++ src/components/Overview.tsx
@@ -1,11 +1,8 @@
-//import pic from "../assets/placeholder.png";
-
 export function Overview() {
   return (
     <div className="flex justify-around mt-8">
-      <img src={/*pic*/} alt="placeholder" className="bg-green-700">
-        PlaceHolder
-      </img>
+      {/*<img src={design} alt="placeholder" className="bg-green-700" />
+       */}
       <p>Hi I'm Philip Stoyanov</p>
     </div>
   );
